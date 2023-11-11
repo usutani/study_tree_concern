6704f8cf9eb91cace43638f31c306f011aa08412
Yasuhiro Usutani <y_usutani@mac.com>
2023-11-11 17:30:26 +0900

    外部キーを追加する

--- db/migrate/20231111082857_create_pins.rb
+++ db/migrate/20231111082857_create_pins.rb
@@ -2,6 +2,7 @@ class CreatePins < ActiveRecord::Migration[7.0]
   def change
     create_table :pins do |t|
       t.string :name
+      t.references :parent, foreign_key: { to_table: :pins }
 
       t.timestamps
     end
